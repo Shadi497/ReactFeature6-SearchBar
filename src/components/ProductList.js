a34db6faf0e83482db20895e415dc56fbbf4ab5e
Shadi497 <b.shadi.h@gmail.com>
2021-01-24 15:47:01 +0300

    Search Done

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -5,15 +5,18 @@ import ProductItem from "./ProductItem";
 import SearchBar from "./SearchBar";
 // Data
 import products from "../products";
+import { useState } from "react";
 
 const ProductList = () => {
-  const productList = products.map((product) => (
-    <ProductItem product={product} key={product.id} />
-  ));
+  const [query, setQuery] = useState("");
+
+  const productList = products
+    .filter((product) => product.name.toLowerCase().includes(query))
+    .map((product) => <ProductItem product={product} key={product.id} />);
 
   return (
     <>
-      <SearchBar />
+      <SearchBar sq={setQuery} />
       <ListWrapper>{productList}</ListWrapper>
     </>
   );
